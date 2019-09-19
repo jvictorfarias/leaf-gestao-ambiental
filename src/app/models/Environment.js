d7319cc73720b5c1bf06857585ed1efa10fb77f1
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-19 16:17:16 -0300

    chore: models connected to the database

--- src/app/models/Environment.js
+++ src/app/models/Environment.js
@@ -1,9 +1,14 @@
-import { Model } from 'sequelize';
+import { Model, Sequelize } from 'sequelize';
 
 class Environment extends Model {
   static init(sequelize) {
     super.init(
-      {},
+      {
+        name: Sequelize.STRING,
+        desc: Sequelize.TEXT,
+        institution_id: Sequelize.UUID,
+        department_id: Sequelize.UUID,
+      },
       {
         sequelize,
       }
---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+import Technician from '../app/models/Technician';
+import Institution from '../app/models/Institution';
+import Department from '../app/models/Department';
+import Environment from '../app/models/Environment';
+
+const models = [Technician, Institution, Department, Environment];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  // Connection with the database e load models
+  init() {
+    this.connection = new Sequelize(databaseConfig); // Start connection(init models)
+    models.map(model => model.init(this.connection)); // Init connection on all models
+  }
+}
+
+export default new Database();
