24460c94a032a8ab9109e26eea6784f9422c63ff
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-10-01 23:31:07 -0300

    chore: migrations for environment aspects

---
+++ src/database/migrations/20191002012246-create-cause.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(() => {
+        return queryInterface.createTable('causes', {
+          id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.literal('uuid_generate_v4()'),
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          desc: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+          },
+          impact_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'impacts',
+              key: 'id',
+            },
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+        });
+      });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('causes');
+  },
+};
