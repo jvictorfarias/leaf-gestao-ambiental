efeffae7566b1e19951c13246944f2aa84f02804
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-10-07 10:40:31 -0300

    dev: basic init script

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo apt install yarn -y;
 yarn install;
-sudo apt install docker-composer docker.io;
-yarn sequelize db:migrate;
+sudo apt install docker-composer docker.io -y;
+docker-compose up;
+yarn sequelize db:create && yarn sequelize db:migrate;
 yarn dev;
