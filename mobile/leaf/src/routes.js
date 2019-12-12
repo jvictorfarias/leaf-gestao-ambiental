4da51686b1a736e056a1023c8aab68c8639b7f25
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-12-12 11:47:31 -0300

    chore: integrating  navigators

--- mobile/leaf/src/routes.js
+++ mobile/leaf/src/routes.js
@@ -10,50 +10,54 @@ import Impacts from './pages/Impacts';
 import Questions from './pages/Questions';
 import Result from './pages/Result';
 
-const routes = createAppContainer(
-  createSwitchNavigator({ Login, Main }),
-  createBottomTabNavigator(
-    {
-      Aspects,
-      Impacts,
-      Questions,
-      Result,
-    },
-    {
-      defaultNavigationOptions: ({ navigation }) => ({
-        tabBarIcon: ({ focused, tintColor }) => {
-          const { routeName } = navigation.state;
-          const IconComponent = Ionicons;
-          // eslint-disable-next-line no-param-reassign
-          if (routeName === 'Aspects') {
-            return (
-              <IconComponent name="ios-clipboard" size={25} color={tintColor} />
-            );
-          }
-          if (routeName === 'Impacts') {
-            return (
-              <IconComponent name="ios-flash" size={25} color={tintColor} />
-            );
-          }
-          if (routeName === 'Questions') {
-            return <IconComponent name="md-help" size={25} color={tintColor} />;
-          }
-          if (routeName === 'Result') {
-            return (
-              <IconComponent name="md-list-box" size={25} color={tintColor} />
-            );
-          }
+const entry = createSwitchNavigator({ Login, Main });
 
+const home = createBottomTabNavigator(
+  {
+    Aspects,
+    Impacts,
+    Questions,
+    Result,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        const IconComponent = Ionicons;
+        // eslint-disable-next-line no-param-reassign
+        if (routeName === 'Aspects') {
+          return (
+            <IconComponent name="ios-clipboard" size={25} color={tintColor} />
+          );
+        }
+        if (routeName === 'Impacts') {
+          return <IconComponent name="ios-flash" size={25} color={tintColor} />;
+        }
+        if (routeName === 'Questions') {
           return <IconComponent name="md-help" size={25} color={tintColor} />;
-        },
-      }),
-      tabBarOptions: {
-        activeTintColor: '#00ca14',
-        inactiveTintColor: 'gray',
-        style: { backgroundColor: '#eee' },
+        }
+        if (routeName === 'Result') {
+          return (
+            <IconComponent name="md-list-box" size={25} color={tintColor} />
+          );
+        }
+
+        return <IconComponent name="md-help" size={25} color={tintColor} />;
       },
-    }
-  )
+    }),
+    tabBarOptions: {
+      activeTintColor: '#00ca14',
+      inactiveTintColor: 'gray',
+      style: { backgroundColor: '#eee' },
+    },
+  }
 );
+
+const AppNavigator = createSwitchNavigator({
+  entry,
+  home,
+});
+
+const routes = createAppContainer(AppNavigator);
 
 export default routes;
